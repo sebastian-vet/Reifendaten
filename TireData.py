4de1b74f8dfb9a5344e4ae51bd58aeb21c51ed62
Sebastian Vetter <Sebastianvetter@outlook.de>
2019-10-29 21:09:06 +0100

    Der "Auswahlmechanismus" für die "Situationen" wurde maßgeblich verändert bzw. verbessert. Aktueller Code sollte laufen.
    ToDo:
    - Für alle Druckniveaus anpassen
    - Richtige Beispieldaten raussuchen
    - weitere Grafiken erstellen
    - Interaktiver Import
    - Den ganzen Spaß noch mal für laterale Daten machen
    - Plausibilitätsprüfung
    - Automatisches exportieren und speichern der Grafkien als jpg. oder ähnliches

--- TireData.py
+++ TireData.py
@@ -1,5 +1,5 @@
 """Prerequesites"""
-'Prerequesites'
+
 import pandas as pd
 import matplotlib.pyplot as plt
 class Tire:
@@ -10,24 +10,30 @@ class Tire:
         self.data=data
         self.pressure_keys=pressure_keys
         self.sortedLonData = {pressure_keys[0]:[]}
-        self.index=[]
+        self.bool_index=[]
 
     def sortLonData(self, ia_keys, fz_keys,sa_keys, iaLowerBorder, iaUpperBorder, fzLowerBorder, fzUpperBorder, slipAngleLowerBorder, slipAngleUpperBorder): # Sortiert die Daten nach "Situationen".
         z = 0
         for i in range(len(self.pressure_keys)):
             #self.sortedLonData[pressure_keys[i]]=pd.DataFrame()
             df_new=[]
-
             for sa_index in range(len(sa_keys)):
                 sa = (self.data[self.pressure_keys[i]]["SA"] < slipAngleUpperBorder[sa_index]) & (self.data[self.pressure_keys[i]]["SA"] > slipAngleLowerBorder[sa_index])
                 for fz_index in range(len(fz_keys)):
                     fz = (self.data[self.pressure_keys[i]]["FZ"] < fzUpperBorder[fz_index]) & (self.data[self.pressure_keys[i]]["SA"] >fzLowerBorder[fz_index])
                     for ia_index in range(len(ia_keys)):
                         ia = (self.data[self.pressure_keys[i]]["IA"] < iaUpperBorder[ia_index]) & (self.data[self.pressure_keys[i]]["SA"] > iaLowerBorder[ia_index])
+                        bool_index=[]
 
-                        index = (sa==fz) & (sa == ia) & (ia == fz) & (sa == True) & (fz == True) & (ia == True)
-                        df_new.append(pd.DataFrame(self.data[pressure_keys[i]][index]))
-                        self.index.append(index)
+                        for m,n,b in zip(sa,fz,ia):
+                            if (m==n) &(m==b) & (b==n):
+                                bool_index.append(True)
+                            else:
+                                bool_index.append(False)
+
+                        #bool_index = (sa==fz) & (sa == ia) & (ia == fz) & (sa == True) & (fz == True) & (ia == True)
+                        df_new.append(pd.DataFrame(self.data[pressure_keys[i]][bool_index]))
+                        self.bool_index.append(bool_index)
                         z=z+1
                         
 
@@ -35,36 +41,33 @@ class Tire:
            # keyTuple = tuple(keyList)
             self.sortedLonData[pressure_keys[i]] = pd.concat(df_new, keys=['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37'])
 
-    #def plotLonData(self, ia keys):       
-    #     z = 
-    #    keyRange = range(z)
-    #        keyList = [*keyRange]
-    #        keyString=[str(j) for j in keyList]
-    #    plt.figure(1)
-    #    for i in range(len(self.pressure_keys[i])):
-    #        plt.plot(self.sortedLonData[self.pressure_keys[i]].loc['0']['SL'],self.sortedLonData['12PSI'].loc['0']['FX'],label=self.pressure_keys[i],'o')
+    def plotLonData(self, ia_keys):
+        z = list(self.sortedLonData["10PSI"].index.get_level_values(0))
+        z = list(dict.fromkeys(z))
+        plt.figure(1)
+        for i in range(len(self.pressure_keys)):
+            plt.plot(self.sortedLonData[self.pressure_keys[i]].loc['0']['SR'],self.sortedLonData[self.pressure_keys[i]].loc['0']['FX'],label=self.pressure_keys[i])
 
-    #    plt.xlabel('Slip Ratio [-]')
-    #    plt.yLabel('FX [N]')
-    #    plt.title('FX over Slip Ratio at ...N normal force and 0° inclination angle')
-    #    plt.legend()
-    #    plt.show()
-    #    plt.figure(2)
-    #    for j in range(len(ia_keys))
-    #        plt.plot(hoosier13.sortedLonData['10PSI'].loc['0']['SL'],hoosier13.sortedLonData['10PSI'].loc['0']['FX'],label=ia_keys[j],'o')
+        plt.xlabel('Slip ratio [-]')
+        plt.ylabel('Fx [N]')
+        plt.title('Fx over slip ratio at -1557N normal force and 0° inclination angle')
+        plt.legend()
+        plt.show()
+        plt.figure(2)
+        for j in range(len(ia_keys)):
+            plt.plot(hoosier13.sortedLonData['10PSI'].loc[z[j]]['SR'],hoosier13.sortedLonData['10PSI'].loc[z[j]]['FX'],label=ia_keys[j])
         
-    #    plt.xlabel('Slip Ratio [-]')
-    #    plt.yLabel('FX [N]')
-    #    plt.title('FX over Slip Ratio for different inclination angles at 10 PSI and -1557 N normal force')
-    #    plt.legend()
-    #    plt.show()
+        plt.xlabel('Slip ratio [-]')
+        plt.ylabel('Fx [N]')
+        plt.title('Fx over slip ratio for different inclination angles at 10 psi and -1557 N normal force')
+        plt.legend()
+        plt.show()
 
    
 
 
 
 """Data import"""
-'Data import'
 df = pd.read_table("Grenzen.txt",header=None)
 keys = df[0].tolist()
 lowerBorders = df[1].tolist()
@@ -85,4 +88,4 @@ hoosier13.sortLonData(key_dict["ia_keys"],key_dict["fz_keys"],key_dict["sa_keys"
 #print(hoosier13.sortedLonData)
 #print(hoosier13.index)
 
-#hoosier13.plotLonData(key_dict["ia_keys"])
+hoosier13.plotLonData(key_dict["ia_keys"])
