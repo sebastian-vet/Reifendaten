939301fc33480c3e18547f79a65a2d69a2ab754f
Sebastian Vetter <Sebastianvetter@outlook.de>
2019-11-08 22:51:54 +0100

    Sortierung gefixt Import mehrerer Reifen ist jetzt möglich Automatischer Export der Plots in pdfs Mehr Plots

--- Tire.py
+++ Tire.py
@@ -6,14 +6,15 @@ import os
 class Tire:
     """ In der Klasse Tire werden alle Informationen zu einem Reifen gespeichert. """
 
-    def __init__(self, size, data, pressure_keys):
-        self.size = size
-        self.data = data
+    def __init__(self, data_braking, data_cornering,  pressure_keys):
+        self.data_braking = data_braking
+        self.data_cornering = data_cornering
         self.pressure_keys = pressure_keys
         self.sortedLonData = {pressure_keys[0]: []}
+        self.sortedLatData = {pressure_keys[0]: []}
         self.bool_index = []
 
-    # Sortiert die Daten nach "Situationen".
+    # Sortiert die Braking Daten nach "Situationen".
     def sortLonData(self, ia_keys, fz_keys, sa_keys, iaLowerBorder, 
                     iaUpperBorder, fzLowerBorder, fzUpperBorder, 
                     slipAngleLowerBorder, slipAngleUpperBorder):
@@ -23,20 +24,20 @@ class Tire:
             df_new = []
             for sa_index in range(len(sa_keys)):
                 sa = 0
-                sa = (self.data[pressure_key]["SA"] < slipAngleUpperBorder[sa_index]) & (
-                    self.data[pressure_key]["SA"] > slipAngleLowerBorder[sa_index])
+                sa = (self.data_braking[pressure_key]["SA"] < slipAngleUpperBorder[sa_index]) & (
+                    self.data_braking[pressure_key]["SA"] > slipAngleLowerBorder[sa_index])
                 for fz_index in range(len(fz_keys)):
                     fz = 0
-                    fz = (self.data[pressure_key]["FZ"] < fzUpperBorder[fz_index]) & (
-                        self.data[pressure_key]["SA"] > fzLowerBorder[fz_index])
+                    fz = (self.data_braking[pressure_key]["FZ"] < fzUpperBorder[fz_index]) & (
+                        self.data_braking[pressure_key]["FZ"] > fzLowerBorder[fz_index])
                     for ia_index in range(len(ia_keys)):
                         ia = 0
-                        ia = (self.data[pressure_key]["IA"] < iaUpperBorder[ia_index]) & (
-                            self.data[pressure_key]["SA"] > iaLowerBorder[ia_index])
+                        ia = (self.data_braking[pressure_key]["IA"] < iaUpperBorder[ia_index]) & (
+                            self.data_braking[pressure_key]["IA"] > iaLowerBorder[ia_index])
                         bool_index = []
 
                         for m, n, b in zip(sa, fz, ia):
-                            if m,n,b:
+                            if m & n & b:
                                 bool_index.append(True)
                             else:
                                 bool_index.append(False)
@@ -46,7 +47,7 @@ class Tire:
                         bool_series = pd.Series(bool_index, name='bool')
                         df_loop = pd.DataFrame()
                         df_loop = pd.DataFrame(
-                            self.data[pressure_key][bool_series])
+                        self.data_braking[pressure_key][bool_series])
 
                         # for index_index in range(len(bool_index)):
                         #    if bool_index[index_index]:
@@ -63,28 +64,156 @@ class Tire:
                                                 '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37'])
             self.sortedLonData[pressure_key] = df_finish
 
-    def plotLonData(self, ia_keys):
+    def plotLonData(self, ia_keys, tireName):
         z = list(self.sortedLonData["10PSI"].index.get_level_values(0))
         z = list(dict.fromkeys(z))
-        plt.figure(1)
+        plt.figure()
         for i in range(len(self.pressure_keys)):
             plt.plot(self.sortedLonData[self.pressure_keys[i]].loc['0']['SL'],
-                     self.sortedLonData[self.pressure_keys[i]].loc['0']['FX'], label=self.pressure_keys[i])
+                     self.sortedLonData[self.pressure_keys[i]].loc['0']['FX'], label=self.pressure_keys[i], marker='.', linestyle = "None")
 
         plt.xlabel('Slip ratio [-]')
         plt.ylabel('Fx [N]')
         plt.title(
-            'Fx over slip ratio at -1557N normal force and 0° inclination angle')
+            'Fx over slip ratio for different pressures at -1557N normal force and 0° inclination angle')
         plt.legend()
-        plt.show()
-        plt.figure(2)
+        #plt.show()
+        plt.savefig("Figures/"+tireName+"/FX_over_slipRatio_for_p.pdf", bbox_inches = 'tight')
+        plt.close
+        plt.figure()
         for j in range(len(ia_keys)):
             plt.plot(self.sortedLonData['10PSI'].loc[z[j]]['SL'],
-                     self.sortedLonData['10PSI'].loc[z[j]]['FX'], label=ia_keys[j])
+                     self.sortedLonData['10PSI'].loc[z[j]]['FX'], label=ia_keys[j], marker='.', linestyle = "None")
 
         plt.xlabel('Slip ratio [-]')
         plt.ylabel('Fx [N]')
         plt.title(
             'Fx over slip ratio for different inclination angles at 10 psi and -1557 N normal force')
         plt.legend()
-        plt.show()
+        #plt.show()
+        plt.savefig("Figures/"+tireName+"/FX_over_slipRatio_for_ia.pdf", bbox_inches = 'tight')
+        plt.close
+
+        plt.figure()
+        for j in range(len(ia_keys)):
+            plt.plot(self.sortedLonData['10PSI'].loc[z[j]]['SL'],
+                     self.sortedLonData['10PSI'].loc[z[j]]['TSTC'], label=ia_keys[j], marker='.', linestyle = "None")
+
+        plt.xlabel('Slip ratio [-]')
+        plt.ylabel('T [°C]')
+        plt.title(
+            'T over slip ratio for different inclination angles at 10 psi and -1557 N normal force')
+        plt.legend()
+        #plt.show()
+        plt.savefig("Figures/"+tireName+"/T_over_slipRatio_for_ia.pdf", bbox_inches = 'tight')
+        plt.close
+
+        #plt.plot(self.sortedLonData['10PSI'].loc["0"]['FZ'],
+        #             self.sortedLonData['10PSI'].loc["0"]['FX'], label=ia_keys[j])
+
+        #plt.xlabel('Slip ratio [-]')
+        #plt.ylabel('Fx [N]')
+        #plt.title(
+        #    'Fx over Fz for different slip ratios at 10 psi and -1557 N normal force')
+        #plt.legend()
+        ##plt.show()
+        #plt.savefig("Figures/FX_over_slipRatio_for_ia.pdf", bbox_inches = 'tight')
+
+
+
+
+    # Sortiert die Cornering Daten nach "Situationen".
+    def sortLatData(self, ia_keys, fz_keys, sa_keys, iaLowerBorder, 
+                    iaUpperBorder, fzLowerBorder, fzUpperBorder, 
+                    slipAngleLowerBorder, slipAngleUpperBorder):
+        for pressure_key in self.pressure_keys:
+            df_new = []
+            for fz_index in range(len(fz_keys)):
+                fz = 0
+                fz = (self.data_cornering[pressure_key]["FZ"] < fzUpperBorder[fz_index]) & (
+                       self.data_cornering[pressure_key]["FZ"] > fzLowerBorder[fz_index])
+                for ia_index in range(len(ia_keys)):
+                    ia = 0
+                    ia = (self.data_cornering[pressure_key]["IA"] < iaUpperBorder[ia_index]) & (
+                            self.data_cornering[pressure_key]["IA"] > iaLowerBorder[ia_index])
+                    bool_index = []
+
+                    for  n, b in zip(fz, ia):
+                        if  n & b:
+                            bool_index.append(True)
+                        else:
+                            bool_index.append(False)
+
+                    bool_series = pd.Series(bool_index, name='bool')
+                    df_loop = pd.DataFrame()
+                    df_loop = pd.DataFrame(
+                    self.data_cornering[pressure_key][bool_series])
+                    df_new.append(df_loop)
+
+
+            df_finish = pd.DataFrame()
+            df_finish = pd.concat(df_new, keys=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16',
+                                                '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37'])
+            self.sortedLatData[pressure_key] = df_finish
+
+
+    def plotLatData(self, ia_keys, fz_keys, sa_keys, tireName):
+            z = list(self.sortedLatData["10PSI"].index.get_level_values(0))
+            z = list(dict.fromkeys(z))
+            plt.figure()
+            for i in range(len(self.pressure_keys)):
+                plt.plot(self.sortedLatData[self.pressure_keys[i]].loc["0"]['SA'],
+                         self.sortedLatData[self.pressure_keys[i]].loc["0"]['FY'], label=self.pressure_keys[i], marker='.', linestyle = "None")
+
+            plt.xlabel('Slip angle [°]')
+            plt.ylabel('Fy [N]')
+            plt.title(
+                'Fy over slip angle for different pressures at -1557N normal force and 0° inclination angle')
+            plt.legend()
+            #plt.show()
+            plt.savefig("Figures/"+tireName+"/FY_over_slipAngle_for_p.pdf", bbox_inches = 'tight')
+            plt.close
+
+
+            plt.figure()
+            for j in range(len(ia_keys)):
+                plt.plot(self.sortedLatData['10PSI'].loc[z[j]]['SA'],
+                         self.sortedLatData['10PSI'].loc[z[j]]['FY'], label=ia_keys[j], marker='.', linestyle = "None")
+
+            plt.xlabel('Slip angle [°]')
+            plt.ylabel('Fy [N]')
+            plt.title(
+                'Fy over slip angle for different inclination angles at 10 psi and -1557 N normal force')
+            plt.legend()
+            #plt.show()
+            plt.savefig("Figures/"+tireName+"/FY_over_slipAngle_for_ia.pdf", bbox_inches = 'tight')
+            plt.close
+
+            plt.figure()
+            for j in range(len(ia_keys)):
+                plt.plot(self.sortedLatData['10PSI'].loc[z[j]]['SA'],
+                     self.sortedLatData['10PSI'].loc[z[j]]['TSTC'], label=ia_keys[j], marker='.', linestyle = "None")
+
+            plt.xlabel('Slip angle [°]')
+            plt.ylabel('T [°C]')
+            plt.title(
+                'T over slip angle for different inclination angles at 10 psi and -1557 N normal force')
+            plt.legend()
+            #plt.show()
+            plt.savefig("Figures/"+tireName+"/T_over_slipAngle_for_ia.pdf", bbox_inches = 'tight')
+            plt.close
+
+            plt.figure()
+            for j in range(len(fz_keys)):
+                plt.plot(self.sortedLatData['10PSI'].loc[z[j*3]]['SA'],
+                     self.sortedLatData['10PSI'].loc[z[j*3]]['MZ'], label=fz_keys[j], marker='.', linestyle = "None")
+
+            plt.xlabel('Slip angle [°]')
+            plt.ylabel('Mz [Nm]')
+            plt.title(
+                'Mz over slip angle for different normal loads at 10 psi and inclination angle = 0')
+            plt.legend()
+            #plt.show()
+            plt.savefig("Figures/"+tireName+"/Mz_over_slipAngle_for_fz.pdf", bbox_inches = 'tight')
+            plt.close
+
--- TireData.py
+++ TireData.py
@@ -17,33 +17,53 @@ lowerBorder_dict = {
 upperBorder_dict = {
     "ia_uBorder": upperBorders[0:3], "fz_uBorder": upperBorders[3:7], "sa_uBorder": upperBorders[7:]}
 
-# Import Braking files
-files_braking = os.listdir("Data/Braking")
-files_braking = [('Data/Braking/' + i) for i in files_braking]
+numOfTire_str = input ("Wie viele Reifen sollen analysiert werden?")
+numOfTire_int = int(numOfTire_str)
 
-# Import Cornering Files
+tire_dict={}
+for numTire in range(numOfTire_int):
+    tireName = "tire" + str(numTire)
+    dir_braking = "Data/" + tireName + "/Braking/"
+    # Import Braking files
+    files_braking = os.listdir(dir_braking)
+    files_braking = [(dir_braking + i) for i in files_braking]
 
+    # Import Cornering Files
+    dir_cornering = "Data/" + tireName + "/Cornering/"
+    files_cornering = os.listdir(dir_cornering)
+    files_cornering = [(dir_cornering + i) for i in files_cornering]
 
-# Import pressure levels
-pressure_keys_df = pd.read_table("Druck.txt", header=None)
-pressure_keys = pressure_keys_df[0].tolist()
-data = {pressure_keys[0]: 0}
-
-# Import raw data
-for i in range(len(files_braking)):
-    data[pressure_keys[i]] = pd.read_table(
-        files_braking[i], header=[1], skiprows=[2])
-
-# Create new object of class Tire and use the object function to sort data in "situations"
-hoosier13 = Tire(13, data, pressure_keys)
-hoosier13.sortLonData(key_dict["ia_keys"], key_dict["fz_keys"], key_dict["sa_keys"], 
-                      lowerBorder_dict["ia_lBorder"], upperBorder_dict["ia_uBorder"],
-                      lowerBorder_dict["fz_lBorder"], upperBorder_dict["fz_uBorder"], 
-                      lowerBorder_dict["sa_lBorder"], upperBorder_dict["sa_uBorder"])
-
-#hoosier13.sortLatData(key_dict["ia_keys"],key_dict["fz_keys"],key_dict["sa_keys"], lowerBorder_dict["ia_lBorder"], upperBorder_dict["ia_uBorder"], lowerBorder_dict["fz_lBorder"], upperBorder_dict["fz_uBorder"], lowerBorder_dict["sa_lBorder"], upperBorder_dict["sa_uBorder"] )
+    # Import pressure levels
+    pressure_keys_df = pd.read_table("Druck.txt", header=None)
+    pressure_keys = pressure_keys_df[0].tolist()
+    data_braking = {pressure_keys[0]: 0}
+    data_cornering = {pressure_keys[0]: 0}
 
 
-# Plot sorted data
-# hoosier13.plotLonData(key_dict["ia_keys"])
-# hoosier13.plotLatData(key_dict["ia_keys"])
+    # Import raw data
+    for i in range(len(files_braking)):
+        data_braking[pressure_keys[i]] = pd.read_table(
+            files_braking[i], header=[1], skiprows=[2])
+
+    for i in range(len(files_cornering)):
+        data_cornering[pressure_keys[i]] = pd.read_table(
+            files_cornering[i], header=[1], skiprows=[2])
+
+    # Create new object of class Tire and use the object function to sort data in "situations"
+    
+    tire_dict[tireName] = Tire(data_braking, data_cornering, pressure_keys)
+    tire_dict[tireName].sortLonData(key_dict["ia_keys"], key_dict["fz_keys"], key_dict["sa_keys"], 
+                          lowerBorder_dict["ia_lBorder"], upperBorder_dict["ia_uBorder"],
+                          lowerBorder_dict["fz_lBorder"], upperBorder_dict["fz_uBorder"], 
+                          lowerBorder_dict["sa_lBorder"], upperBorder_dict["sa_uBorder"])
+
+    tire_dict[tireName].sortLatData(key_dict["ia_keys"], key_dict["fz_keys"], key_dict["sa_keys"], 
+                          lowerBorder_dict["ia_lBorder"], upperBorder_dict["ia_uBorder"],
+                          lowerBorder_dict["fz_lBorder"], upperBorder_dict["fz_uBorder"], 
+                          lowerBorder_dict["sa_lBorder"], upperBorder_dict["sa_uBorder"])
+
+
+    # Plot sorted data
+    os.makedirs("Figures/"+tireName)
+    tire_dict[tireName].plotLonData(key_dict["ia_keys"], tireName)
+    tire_dict[tireName].plotLatData(key_dict["ia_keys"], key_dict["fz_keys"],key_dict["sa_keys"],tireName)
